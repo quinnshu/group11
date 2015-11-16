f5578734dfb98e0a1a6b5a62fb79f3e307993da8
Quinn R. Shumaker <quinnshu@ougl-33-a.eplt.washington.edu>
2015-11-15 18:16:25 -0800

    resolves #6 and resolves #7

--- js/app.js
+++ js/app.js
@@ -1,24 +1,42 @@
 angular.module('SignupApp', [])
-	// requirement of date of birth for users to be older than 13
-	.directive('olderThan13', function() {
-		return {
-			require: 'ngModel',
-			link: function(scope, elem, attrs, controller) {
-				controller.$validators.olderthan13 = function(modelValue) {
-
+		// requirement of date of birth for users to be older than 13
+		.directive('olderThan13', function () {
+			return {
+				require: 'ngModel',
+				link: function (scope, elem, attrs, controller) {
+					controller.$validators.olderThan13 = function (modelValue) {
+						return (moment().diff(moment(modelValue), 'years') > 13);
+					}
 				}
 			}
-		}
-	})
-	.controller('SignupController', function($scope) {
-		'use strict';
-		// shows successful signup message
-		$('.submit').click(function() {
-			$('.alert').show();
-		});
+		})
+		//directive to ensure "password" and "confirm password" fields match
+		.directive('passwordVerify', function () {
+			return {
+				require: "ngModel",
+				link: function (scope, element, attrs, ctrl) {
+					ctrl.$parsers.unshift(function (viewValue) {
+						var origin = scope.$eval(attrs["passwordVerify"]);
+						if (origin !== viewValue) {
+							ctrl.$setValidity("passwordVerify", false);
+							return undefined;
+						} else {
+							ctrl.$setValidity("passwordVerify", true);
+							return viewValue;
+						}
+					});
 
-		// hides successful signup message
-		$('.reset').click(function() {
-			$('.alert').hide();
-		});
+				}
+			};
+		}).controller('SignupController', function ($scope) {
+	'use strict';
+	// shows successful signup message
+	$('.submit').click(function () {
+		$('.alert').show();
 	});
+
+	// hides successful signup message
+	$('.reset').click(function () {
+		$('.alert').hide();
+	});
+});
